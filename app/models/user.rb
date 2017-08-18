a2c48410595472316ba641642c80b4ac1f14236d
Butch Marshall <butch.a.marshall@gmail.com>
2017-08-17 21:26:25 -0400

    - Added models for game tracking

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :trackable, :validatable
+	devise :database_authenticatable, :registerable, :trackable, :validatable
+
+	has_many :game_users
+	has_many :games, :through => :game_users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119003939) do
+ActiveRecord::Schema.define(version: 20170818012312) do
+
+  create_table "game_users", force: :cascade do |t|
+    t.integer  "game_id"
+    t.integer  "user_id"
+    t.integer  "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "game_users", ["game_id"], name: "index_game_users_on_game_id"
+  add_index "game_users", ["user_id"], name: "index_game_users_on_user_id"
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "played_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
